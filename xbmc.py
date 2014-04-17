fc6c2c7be3164820550c9a89e64020fdcb333090
Nick Tones <nicktones@icloud.com>
2014-04-17 02:45:28 +0100

    Proof of concept

--- xbmc.py
+++ xbmc.py
@@ -1,26 +1,50 @@
 import re
+import json
+import requests
 
-WORDS = ["GLADOS", "PLAY", "PAUSE"]
+xbmcName = ""
+xbmcIp = ""
+xbmcPort = ""
+xbmcUsername = ""
+xbmcPassword = ""
+
+WORDS = [xbmcName, "PLAY", "PAUSE", "STOP"]
 
 def handle(text, mic, profile):
     """
         Responds to user-input to control XBMC.
+        
+        Current supports:
+            -Pause / Play
+            -Stop
 
         Arguments:
-        text -- user-input, typically transcribed speech
-        mic -- used to interact with the user (for both input and output)
-        profile -- contains information related to the user (e.g., phone number)
+        	text -- user-input, typically transcribed speech
+       		mic -- used to interact with the user (for both input and output)
+        	profile -- contains information related to the user (e.g., phone number)
     """
-    
 
-    mic.say(text)
-
+    if bool(re.search(r'\b{0}\b'.format("PAUSE"), text, re.IGNORECASE)) or bool(re.search(r'\b{0}\b'.format("PLAY"), text, re.IGNORECASE)):
+        mic.say("PLAY PAUSE")
+        data = {'jsonrpc':'2.0','method':'Player.PlayPause','params':{'playerid':1},'id':1}
+        xbmcUrl = "http://"+xbmcUsername+":"+xbmcPassword+"@"+xbmcIp+":"+xbmcPort+"/json?request="
+        data_json = json.dumps(data)
+        r = requests.post(xbmcUrlTest, data_json)
+    elif bool(re.search(r'\b{0}\b'.format("STOP"), text, re.IGNORECASE)):
+        mic.say("STOP")
+        data = {'jsonrpc':'2.0','method':'Player.Stop','params':{'playerid':1},'id':1}
+        data_json = json.dumps(data)
+        xbmcUrl = "http://"+xbmcUsername+":"+xbmcPassword+"@"+xbmcIp+":"+xbmcPort+"/json?request="
+        r = requests.post(xbmcUrlTest, data_json)
+    else:
+        mic.say("Sorry, I don't know how to do that yet")
 
 def isValid(text):
     """
         Returns True if the text is related to xbmc.
 
         Arguments:
-        text -- user-input, typically transcribed speech
+        	text -- user-input, typically transcribed speech
     """
-    return bool(re.search(r'\bglados\b', text, re.IGNORECASE))
+    
+    return bool(re.search(r'\b{0}\b'.format(xbmcName), text, re.IGNORECASE))
